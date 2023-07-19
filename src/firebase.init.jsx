7e34c1cbfacaebe045059b40a0785f1cb4089754
nargis21 <nargisakther1933@gmail.com>
2023-07-20 00:10:26 +0600

    firebase added

---
+++ src/firebase.init.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa3UH4qaGCKEl8a8gTXWACO5JH--8Pgo4",
+  authDomain: "house-hunter-92dfa.firebaseapp.com",
+  projectId: "house-hunter-92dfa",
+  storageBucket: "house-hunter-92dfa.appspot.com",
+  messagingSenderId: "933634601225",
+  appId: "1:933634601225:web:11625dda33e21d08ff23d8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
